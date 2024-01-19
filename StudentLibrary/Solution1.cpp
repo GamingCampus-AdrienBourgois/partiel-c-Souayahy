a74bffeb91369dc00da40e1abf5a6d37c63578d3
Souayahy <115532933+Souayahy@users.noreply.github.com>
2024-01-19 14:59:16 +0100

    Update Solution1.cpp

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -7,46 +7,6 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	
-	/*std::cout << "Choose your value : " << std::endl;
-	std::cin >> _value;
-
-	std::cout << "Type the ID of the unit you want to convert from :  " << std::endl;
-	std::cout << "1 CELSIUS - 2 FAHRENHEIT - 3 KELVIN " << std::endl;
-	int fromID;
-	std::cin >> fromID;
-
-	switch(fromID) 
-	{
-		case 1 :
-			_from = TemperatureUnits::CELSIUS;
-			break;
-		case 2 :
-			_from = TemperatureUnits::FAHRENHEIT;
-			break;
-		case 3 :
-			_from = TemperatureUnits::KELVIN;
-			break;
-	}
-
-	std::cout << "Choose the unit you want to convert to : " << std::endl;
-	std::cout << "1 CELSIUS - 2 FAHRENHEIT - 3 KELVIN " << std::endl;
-	int toID;
-	std::cin >> toID;
-
-	switch(toID)
-	{
-		case 1:
-			_to = TemperatureUnits::CELSIUS;
-			break;
-		case 2:
-			_to = TemperatureUnits::FAHRENHEIT;
-			break;
-		case 3:
-			_to = TemperatureUnits::KELVIN;
-			break;
-	}*/
-
 	if ((_from == TemperatureUnits::CELSIUS) && (_to == TemperatureUnits::FAHRENHEIT))
 	{
 		_value = (9.0 / 5.0) * _value + 32;
