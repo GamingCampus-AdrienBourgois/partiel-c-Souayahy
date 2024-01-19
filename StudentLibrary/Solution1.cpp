d056a96df90834fedcdd657e3af5a775093eba34
Souayahy <115532933+Souayahy@users.noreply.github.com>
2024-01-19 14:15:41 +0100

    Add files via upload

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -14,15 +14,19 @@ float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, Temper
 	std::cout << "1 CELSIUS - 2 FAHRENHEIT - 3 KELVIN " << std::endl;
 	int fromID;
 	std::cin >> fromID;
+	
 
 	switch(fromID) 
 	{
 		case 1 :
 			_from = TemperatureUnits::CELSIUS;
+			break;
 		case 2 :
 			_from = TemperatureUnits::FAHRENHEIT;
+			break;
 		case 3 :
 			_from = TemperatureUnits::KELVIN;
+			break;
 	}
 
 	std::cout << "Choose the unit you want to convert to : " << std::endl;
@@ -34,17 +38,46 @@ float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, Temper
 	{
 		case 1:
 			_to = TemperatureUnits::CELSIUS;
+			break;
 		case 2:
 			_to = TemperatureUnits::FAHRENHEIT;
+			break;
 		case 3:
 			_to = TemperatureUnits::KELVIN;
+			break;
 	}
 
-	if ((_from == TemperatureUnits::CELSIUS) and (_to == TemperatureUnits::FAHRENHEIT))
+	if ((_from == TemperatureUnits::CELSIUS) && (_to == TemperatureUnits::FAHRENHEIT))
 	{
-		_value = 9/5 * _value + 32;
+		_value = (9.0 / 5.0) * _value + 32;
 	}
-	return -1.0f;
+
+	else if ((_from == TemperatureUnits::KELVIN) && (_to == TemperatureUnits::FAHRENHEIT))
+	{
+		_value = (9.0 / 5.0) * (_value - 273.15) + 32;
+	}
+
+	else if ((_from == TemperatureUnits::FAHRENHEIT) && (_to == TemperatureUnits::CELSIUS))
+	{
+		_value = (5.0 / 9.0) * (_value - 32);
+	}
+
+	else if ((_from == TemperatureUnits::CELSIUS) && (_to == TemperatureUnits::KELVIN))
+	{
+		_value = _value + 273.15;
+	}
+
+	else if ((_from == TemperatureUnits::KELVIN) && (_to == TemperatureUnits::CELSIUS))
+	{
+		_value = _value - 273.15;
+	}
+
+	else if ((_from == TemperatureUnits::FAHRENHEIT) && (_to == TemperatureUnits::KELVIN))
+	{
+		_value = (5.0 / 9.0) * (_value - 32) + 273.15;
+	}
+
+	return _value;
 }
 
 #endif
